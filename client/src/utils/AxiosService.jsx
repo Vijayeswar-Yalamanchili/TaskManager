6ca72df6f516bf61ad3c6770294a70b6ca6c0662
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-07-31 16:33:54 +0530

    updated

--- client/src/utils/AxiosService.jsx
+++ client/src/utils/AxiosService.jsx
@@ -4,8 +4,6 @@ let serverBaseURL = import.meta.env.VITE_SERVER_URL
 
 const AxiosService = axios.create({
     baseURL : `${serverBaseURL}`,
-    // baseURL : "http://localhost:6005",
-    // baseURL : 'https://farmkettle.onrender.com',
     headers : {
         "Content-Type" :"application/json",
         Authorization : localStorage.getItem('loginToken')
